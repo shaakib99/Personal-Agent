7fc5d8621596b6ee9fbd585a46928322f9b68830
Wahid Shakib <wsakib87@gmail.com>
2026-06-21 21:28:09 +0600

    added interest service

--- database_service/config.py
+++ database_service/config.py
@@ -1,10 +1,12 @@
 from typing import Literal, Dict, Any
 from database_service.user_service import UserService
 from database_service.preference_service import PreferenceService
+from database_service.interest_service import InterestService
 
-AllowedCollections = Literal['user', 'preference'] #'preferences', 'interests', 'products']
+AllowedCollections = Literal['user', 'preference', 'interest'] #'preferences', 'products']
 AllowedOps = Literal['create', 'update', 'get']
 CollectionToServiceMap: Dict[str, Any] = {
     'user': UserService,
-    'preference': PreferenceService
+    'preference': PreferenceService,
+    'interest': InterestService
 }
--- database_service/interest_service.py
+++ database_service/interest_service.py
@@ -7,30 +7,30 @@ from uuid import uuid4
 # from database_service.config import AllowedOps
 from database_service.user_service import UserService
 
-class CreatePreferenceMetaData(BaseModel):
+class CreateInterestMetaData(BaseModel):
     id: str = Field(None, description='Id of the user, if None, system will set')
     text: str = Field(description='can hold any string data, like complete json text for semantic search. This field does not necessarily has to match the metadata')
-    type: str = Field(None, description='Type of preference')
+    type: str = Field(None, description='Type of Interest')
     created_datetime: str = Field(datetime.now().strftime("%d/%m/%Y, %H:%M:%S"), description='datetime record created')
     user_id: str = Field('', description='prefrence of the user. Id will be coming from user collection')
     user_email: str = Field(description='''email of the user. this field is for ai agent''')
 
 
-class UpdatePreferenceMetaData(BaseModel):
+class UpdateInterestMetaData(BaseModel):
     id: str = Field(description='Id of the user')
-    type: str = Field(None, description='Type of preference')
+    type: str = Field(None, description='Type of Interest')
     text: str = Field(description='can hold any string data, like complete json text for semantic search. This field does not necessarily has to match the metadata')
 
 
-class PreferenceQueryField(BaseModel):
+class InterestQueryField(BaseModel):
     user_email: str = Field(description='''user_email of the user. type: str''')
     user_id: str = Field(None, description='''user_id of the user. For AI agent no need to populate it''')
-    type: str =  Field(None, description='Type of preference')
+    type: str =  Field(None, description='Type of Interest')
 
 
-class PreferenceService:
+class InterestService:
     def __init__(self):
-        self._collection = 'preference'
+        self._collection = 'interest'
         self.database_service = DatabaseService(self._collection)
         self.user_service = UserService()
 
@@ -42,7 +42,7 @@ class PreferenceService:
     
     async def aget(self, data: Dict[str, Any]):
         query = QueryModel.model_validate(data)
-        filter = PreferenceQueryField.model_validate(query.filter)
+        filter = InterestQueryField.model_validate(query.filter)
         if filter.user_email is not None:
             user_query_model = QueryModel(text='', filter={'email': filter.user_email}, limit=1)
             user = await self.user_service.aget(user_query_model.model_dump())
@@ -58,18 +58,19 @@ class PreferenceService:
         return await self.database_service.aget(query)
     
     async def acreate_one(self, metadata: Dict[str, Any]):
-        data = CreatePreferenceMetaData.model_validate(metadata)
+        data = CreateInterestMetaData.model_validate(metadata)
         if data.user_email:
             user_query_model = QueryModel(text='', filter={'email': data.user_email}, limit=1)
             user = await self.user_service.aget(user_query_model.model_dump())
             if user is None or len(user.get('ids')) == 0: return f'Passed user id does not exist'
             data.user_id = user.get('ids')[0][0]
         data.id = str(uuid4())
+        print(data)
         await self.database_service.acreate_one(data.text, data.model_dump(exclude=['text']))
         return await self.aget_one(data.id)
     
     async def aupdate_one(self, metadata: Dict[str, Any]):
-        data = UpdatePreferenceMetaData.model_validate(metadata)
+        data = UpdateInterestMetaData.model_validate(metadata)
         await self.database_service.aupdate_one(data.id, data.text, data.model_dump(exclude=['text', 'id']))
         return await self.aget_one(data.id)
     
@@ -78,13 +79,13 @@ class PreferenceService:
     
     async def get_metadata_json(self, op: str):
         if op == 'create':
-            return [{'field_name': field_name, 'field_description': field_info.description} for field_name, field_info in CreatePreferenceMetaData.model_fields.items()]
+            return [{'field_name': field_name, 'field_description': field_info.description} for field_name, field_info in CreateInterestMetaData.model_fields.items()]
         elif op == 'update':
-            return [{'field_name': field_name, 'field_description': field_info.description} for field_name, field_info in UpdatePreferenceMetaData.model_fields.items()]
+            return [{'field_name': field_name, 'field_description': field_info.description} for field_name, field_info in UpdateInterestMetaData.model_fields.items()]
         elif op == 'get':
             result = [{'field_name': field_name, 'field_description': field_info.description, 'is_required': field_info.is_required()} for field_name, field_info in QueryModel.model_fields.items()]
             available_filter_options = []
-            for field_name, field_info in PreferenceQueryField.model_fields.items():
+            for field_name, field_info in InterestQueryField.model_fields.items():
                 available_filter_options.append({
                         'field_name' : field_name,
                         'description': field_info.description,
